3cd50c90019502b80fe9835572c585ef5a973943
MiguelJorge34 <55849645+MiguelJorge34@users.noreply.github.com>
2023-03-17 10:54:14 +0000

    Update ValidAnagram.java

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -3,17 +3,19 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
+        // cria a tabela para ocorrencia e numero de ocorrencia de chars em S
         HashMap <Character,Integer> mapS = new HashMap <>();
-
+        // cria a tabela para ocorrencia e numero de ocorrencia de chars em T
         HashMap <Character,Integer> mapT = new HashMap <>();
-
+        // insere os chars e o numero de vezes que foram vistos S
         for(int i = 0 ; i < s.length() ; i++){
             mapS.put(s.charAt(i) , mapS.getOrDefault(s.charAt(i) , 0) +1);
         }
+        // insere os chars e o numero de vezes que foram vistos T
         for(int j = 0 ; j < s.length() ; j++){
             mapT.put(t.charAt(j) , mapT.getOrDefault(t.charAt(j) , 0) +1);
         }
-
+        //Percorre as keys de S ve se o T tem essa key e compara as ocorrencias se forem diferentes não é anagrama
         for(Character ch : mapS.keySet()){
             int val1 = mapS.get(ch); 
             int val2 = mapT.getOrDefault(ch,0);
