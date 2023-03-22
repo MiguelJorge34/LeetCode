3295581bd4b6b750c8aa8713b7abd018c7bbc0e1
MiguelJorge34 <55849645+MiguelJorge34@users.noreply.github.com>
2023-03-22 16:19:48 +0000

    Create IntersectionOfTwoLinkedLists.java

---
+++ IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int sizeA = size(headA);
+        int sizeB = size(headB);
+
+        int diferenceSize = Math.abs(sizeA - sizeB);
+        System.out.println(sizeA +" " + sizeB);
+        if(sizeA > sizeB){
+            for(int i = 0; i < diferenceSize; i++){
+                headA = headA.next;
+            }
+        }
+        if(sizeA < sizeB){
+            for(int i = 0; i < diferenceSize; i++){
+                headB = headB.next;
+            }
+        }
+        while(headA != null && headB !=null){
+            if(headA == headB){
+                return headA;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+    }
+    public int size(ListNode node){
+        
+        int size = 0;
+        while(node != null){
+            size++;
+            node = node.next;
+        }
+        return size;
+    }
+}
