afa2c0213e61a36f033bbd96489891df7d89ec7c
MiguelJorge34 <55849645+MiguelJorge34@users.noreply.github.com>
2023-03-22 08:50:29 +0000

    Update InOrderTraversal.java

--- InOrderTraversal.java
+++ InOrderTraversal.java
@@ -22,3 +22,27 @@ class Solution {
         }
     
 }
+//Solução mais facil
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        if(root == null){
+            return res;
+        }
+
+        inOrder(root,res);
+        return res;
+    }
+    public void inOrder(TreeNode root,List<Integer> res){
+        if(root == null) {
+            return;
+        }
+        inOrder(root.left,res);
+        res.add(root.val);
+        inOrder(root.right,res);
+        
+        
+    }
+}
