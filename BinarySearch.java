3d401617fcef25eae53125994c4fa91bff0d2225
MiguelJorge34 <55849645+MiguelJorge34@users.noreply.github.com>
2023-03-22 12:14:35 +0000

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,18 +2,22 @@ class Solution
 {
     public int search(int[] A, int target) 
     {
-      int start = 0;
-		  int end = A.length - 1;
-        
-      while(start <= end){
-        int mid = start + (end - start) / 2;
-        if(target == A[mid])
-				  return mid;
-			  else if(target < A[mid])
-           end = mid - 1;
-        else 
-           start = mid + 1;
-        }
+	int start = 0;
+	int end = A.length - 1;
+        while(start <= end){
+        	int mid = start + (end - start) / 2;
+        	if(target == A[mid]){
+			return mid;
+		}
+		
+		
+		else if(target < A[mid]){
+          		end = mid - 1;
+		}
+		else {
+          	 start = mid + 1;
+        	}
+	}
         return -1;
     }
 }
