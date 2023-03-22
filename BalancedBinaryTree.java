adc479b93770ea65e15f71f2e726a34c9a21bd4c
MiguelJorge34 <55849645+MiguelJorge34@users.noreply.github.com>
2023-03-22 09:23:24 +0000

    Update BalancedBinaryTree.java

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root == null) return true;
-        int hl = getHeight(root.left);
-        int hr = getHeight(root.right);
-        return Math.abs(hr - hl) <= 1 
-            && isBalanced(root.left)
-        && isBalanced(root.right);
+        if(Math.abs(height(root.left) - height(root.right)) > 1) return false;
+        else return isBalanced(root.left) && isBalanced(root.right);
     }
-    
-    private int getHeight(TreeNode root) {
+    public int height(TreeNode root){
         if(root == null) return 0;
-        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+        return Math.max(height(root.left),height(root.right))+1;
     }
 }
