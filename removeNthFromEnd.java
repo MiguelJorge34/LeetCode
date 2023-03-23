37fac197aed0657856e670f2186e450d05603161
MiguelJorge34 <55849645+MiguelJorge34@users.noreply.github.com>
2023-03-23 08:38:58 +0000

    Create removeNthFromEnd.java

---
+++ removeNthFromEnd.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode temp = new ListNode();
+        temp = head;
+
+        ListNode curr = head;
+        
+        int len = 0;
+
+        while (temp != null) {
+            temp = temp.next;
+            len++;
+        }
+        if (len == 1 && n == 1) {
+            return null;
+        }
+        n = len - n - 1;
+        temp = head;
+        if (n < 0) {
+            return head.next;
+        }
+
+        for (int i = 0 ; i < n ; i++){
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        return head;
+    }
+}
