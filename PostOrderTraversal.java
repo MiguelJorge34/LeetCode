45cfdbcfd23d04895f3f158d53d88480d5901405
MiguelJorge34 <55849645+MiguelJorge34@users.noreply.github.com>
2023-03-24 08:38:07 +0000

    Update PostOrderTraversal.java

--- PostOrderTraversal.java
+++ PostOrderTraversal.java
@@ -22,3 +22,27 @@ class Solution {
 
     }
 }
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        if (root == null){
+            return res;
+        }
+
+        postOrderTraversal(root,res);
+        return res;
+    }
+
+    public void postOrderTraversal(TreeNode root,List<Integer>res){
+        if(root==null){
+            return;
+        }
+        postOrderTraversal(root.left,res);
+        postOrderTraversal(root.right,res);
+        res.add(root.val);
+    }
+
+    
+}
