5c652e94448b1d72a893953a2ab51856f74e975e
MiguelJorge34 <55849645+MiguelJorge34@users.noreply.github.com>
2023-03-31 09:04:16 +0100

    Create RemoveDuplicatesSortedList.java

---
+++ RemoveDuplicatesSortedList.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        while(temp != null){
+            while(temp.next != null && temp.next.val == temp.val){
+                temp.next = temp.next.next;
+            }
+            temp = temp.next;
+        }
+        return head;
+
+    }
+
+}
